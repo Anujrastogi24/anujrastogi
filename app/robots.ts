d62ad37d7aab82e5e5693d67476f43fe069f4742
Anuj Rastogi <anujrastogi2442001@gmail.com>
2026-09-21 23:28:23 +0530

    Some change

--- app/robots.ts
+++ app/robots.ts
@@ -6,7 +6,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/_next/", "/api/"],
     },
     sitemap: absoluteUrl("/sitemap.xml"),
     host: siteConfig.url,
